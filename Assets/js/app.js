24dc58c900fff170a6816e5547b6a45fba4df542
HomS <obr_dev@protonmail.com>
2021-01-09 23:33:07 -0600

    Improved principal section, changed color scheme, implemented sass

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,11 +1,40 @@
-const hamburgerBtn= document.querySelector(".hamburger-menu");
-const menuPanel= document.getElementById("MenuPanel");
+const hamburgerBtn = document.querySelector(".hamburger-menu");
+const menuPanel = document.getElementById("MenuPanel");
+// Sections
+const mainPage = document.getElementById("mainPage");
+const skillsPage = document.getElementById("skillsPage");
 
-hamburgerBtn.addEventListener("click",()=>{
-    hamburgerBtn.classList.toggle("active");
-    if(hamburgerBtn.classList.contains("active")){
-       menuPanel.classList.toggle("show-menu");
-    }else{
-        menuPanel.classList.toggle("show-menu");
-    }
+// Menu Options
+const navHome = document.getElementById("nav-home");
+const navSkills = document.getElementById("nav-skills");
+const navWork = document.getElementById("nav-work");
+const navAbout = document.getElementById("nav-about");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.toggle("active");
+  if (hamburgerBtn.classList.contains("active")) {
+    menuPanel.classList.toggle("show-menu");
+    mainPage.classList.toggle("flip-view");
+  } else {
+    menuPanel.classList.toggle("show-menu");
+    mainPage.classList.toggle("flip-view");
+  }
 });
+
+function navigationMenu(menuItem, pageSection) {
+  menuItem.addEventListener("click", () => {
+    let active = document.querySelector(".flip-view");
+    if (active.id != pageSection.id) {
+      active.classList.toggle("hidden");
+    } else {
+      if (pageSection.classList.contains("hidden")) {
+        pageSection.classList.toggle("hidden");
+      } else {
+        console.log(`${pageSection} no contiene hidden`);
+      }
+    }
+  });
+}
+
+navigationMenu(navHome, mainPage);
+navigationMenu(navSkills, skillsPage);
